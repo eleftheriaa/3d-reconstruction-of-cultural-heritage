cfed318ec8afc6960c28277ff48f4f1228b175ef
eleftheriaa <esolomonide@gmail.com>
2026-05-29 11:01:46 +0300

    export pcd

--- pcd_crop_test.py
+++ pcd_crop_test.py
@@ -6,9 +6,9 @@ import numpy as np
 # 2026-04-02_001758
 # outputs\instant-ngp-scene-scale05-grid-1\2048\2026-05-21_164829
 # G:\elefth\shrec\eythimis\outputs\dataset\nerfacto\2026-04-02_110514\config.yml
-config_file = Path("outputs/cube-ngp-eval-interval-8-near35-far55-athre1e-3-grid-lvs-4/instant-ngp/2026-05-26_105014/config.yml")
+config_file = Path("outputs/All_faces_sculpted_primitive/90_1920x1080_relief_heightmap_1_all.obj/instant_ngp_tuned/2026-05-28_173743/config.yml")
 config = yaml.load(config_file.read_text(), Loader=yaml.Loader)   
-config.load_dir = Path("outputs/cube-ngp-eval-interval-8-near35-far55-athre1e-3-grid-lvs-4/instant-ngp/2026-05-26_105014/nerfstudio_models")
+config.load_dir = Path("outputs/All_faces_sculpted_primitive/90_1920x1080_relief_heightmap_1_all.obj/instant_ngp_tuned/2026-05-28_173743/nerfstudio_models")
 config.load_step = None
 config.print_to_terminal()    
 
@@ -55,7 +55,7 @@ pcd = generate_point_cloud(
     std_ratio=10,
 )
 
-output_path = Path("pcd_box/point_cloud_crop_1M.ply")
+output_path = Path("pcd_box/All_faces_sculpted_primitive/90_1920x1080_relief_heightmap_1_all.obj.ply")
 o3d.io.write_point_cloud(str(output_path), pcd)
 
 print(f"Point cloud saved to {output_path}")
